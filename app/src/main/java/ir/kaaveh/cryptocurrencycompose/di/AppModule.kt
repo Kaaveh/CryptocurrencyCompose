5e0ad37cbb65dfd0c34b97c713e6b48cd7558d1a
kaaveh <kaaveh@pm.me>
2021-09-13 09:06:08 +0430

    Implement the coin detail screen

--- app/src/main/java/ir/kaaveh/cryptocurrencycompose/di/AppModule.kt
+++ app/src/main/java/ir/kaaveh/cryptocurrencycompose/di/AppModule.kt
@@ -7,6 +7,7 @@ import dagger.hilt.components.SingletonComponent
 import ir.kaaveh.cryptocurrencycompose.common.Constants.BASE_URL
 import ir.kaaveh.cryptocurrencycompose.data.remote.CoinPaprikaAPI
 import ir.kaaveh.cryptocurrencycompose.data.repository.CoinRepositoryImpl
+import ir.kaaveh.cryptocurrencycompose.domain.repository.CoinRepository
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -17,15 +18,17 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providePaprikaAPI() =
-        Retrofit.Builder()
+    fun providePaprikaApi(): CoinPaprikaAPI {
+        return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(CoinPaprikaAPI::class.java)
+    }
 
     @Provides
     @Singleton
-    fun provideCoinRepository(api: CoinPaprikaAPI) =
-        CoinRepositoryImpl(api)
+    fun provideCoinRepository(api: CoinPaprikaAPI): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
 }
--- app/src/main/java/ir/kaaveh/cryptocurrencycompose/presentation/MainActivity.kt
+++ app/src/main/java/ir/kaaveh/cryptocurrencycompose/presentation/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import ir.kaaveh.cryptocurrencycompose.presentation.coin_detail.CoinDetailState
+import ir.kaaveh.cryptocurrencycompose.presentation.coin_detail.CoinDetailScreen
+import ir.kaaveh.cryptocurrencycompose.presentation.coin_list.CoinListScreen
 import ir.kaaveh.cryptocurrencycompose.presentation.ui.theme.CryptocurrencyComposeTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +22,24 @@ class MainActivity : ComponentActivity() {
             CryptocurrencyComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route,
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ){
+                            CoinListScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ){
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptocurrencyComposeTheme {
-        Greeting("Android")
     }
 }
